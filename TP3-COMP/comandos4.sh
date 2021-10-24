ccc518ce57351319917c4771dd0f6fed925ef6c0
JoaoVictorMagalhaesSouza <joao.souza5@ufv.br>
2021-10-24 10:57:50 -0300

    Mudanca nos .sh

--- TP3-COMP/comandos4.sh
+++ TP3-COMP/comandos4.sh
@@ -1,4 +1,4 @@
 bison -dy melodia.y
 flex melodia.l
-gcc -omelodia lex.yy.c y.tab.c -ll tabela_hash.c tabela_hash.h
+gcc -omelodia lex.yy.c y.tab.c -ll -lm  tabela_hash.c tabela_hash.h
 ./melodia < entrada4.txt
